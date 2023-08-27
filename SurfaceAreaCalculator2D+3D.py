d5b002aa63aac5e50eada985dd1b02fc03504b29
casss77 <143303905+casss77@users.noreply.github.com>
2023-08-26 21:24:33 -0600

    SurfaceAreaCalculator2D+3D.py
    fixed decimal number crashing program and added some style

--- SurfaceAreaCalculator2D+3D.py
+++ SurfaceAreaCalculator2D+3D.py
@@ -1,35 +1,46 @@
+  GNU nano 7.2                                    surface_area_calc.py                                              
+# casonator was here
 # Area calculator of 2D quadilateral:
-import time
+
+from colorama import *
+from time import sleep
+
 Pi = 3.1415
 Delay = 0.005
 
+# function for 2D calcualtion
 def TwoDimAreaCalc():
-    Height = int(input("Enter height: "));time.sleep(Delay)
-    print("Height set to " + str(Height) + ".")
-    Width = int(input("Enter width: "));time.sleep(Delay)
-    print("Radius set to " + str(Width) + ".")
+    Height = float(input("Enter height: " + Fore.GREEN));sleep(Delay)
+    print(Fore.RESET + "Height set to " + Fore.BLUE  + str(Height) + Fore.RESET)
+    Width = float(input("Enter width:" + Fore.GREEN));sleep(Delay)
+    print(Fore.RESET + "Radius set to " + Fore.BLUE  + str(Width) + Fore.RESET)
     Area = Height * Width
-    print ("\nThe area is " + str(Area) + ".")
+    print (Fore.RESET + Style.BRIGHT + "\nThe area is " + Fore.YELLOW +  str(Area) + "\n")
 
+# function for 3D calcualtion
 def ThreeDimAreaCalc():
-    Height2 = int(input("Enter height: "));time.sleep(Delay)
-    print("Height set to " + str(Height2) + ".")
-    Width2 = int(input("Enter width: "));time.sleep(Delay)
-    print("Radius set to " + str(Width2) + ".")
-    Depth = int(input("Enter depth: "));time.sleep(Delay)
-    print("Depth set to " +str(Depth) + ".")
+    Height2 = float(input("Enter height: " + Fore.GREEN));sleep(Delay)
+    print(Fore.RESET + "Height set to " + Fore.BLUE + str(Height2) + Fore.RESET)
+    Width2 = float(input("Enter width: " + Fore.GREEN));sleep(Delay)
+    print(Fore.RESET + "Radius set to " + Fore.BLUE + str(Width2) + Fore.RESET)
+    Depth = float(input("Enter depth: " + Fore.GREEN));sleep(Delay)
+    print(Fore.RESET + "Depth set to " + str(Depth))
     DH = Depth * Height2
     SQAREA = Width2 * DH
-    print ("\nSquare area of 3D right-angled quad: " + str(SQAREA))
+    print (Fore.RESET + Style.BRIGHT + "\nSquare area of 3D right-angled quad: " + Fore.YELLOW + str(SQAREA) + "\n")
 
 def Choose():
-    Choice = int(input("Choose a selection.\n [ 1 ] for square area calculation of a 2D quad, [ 2 ] for square area calculation of a 3D right angle quad: "))
+
+    Choice = int(input("Choose a selection.\n " + Fore.CYAN + "[ 1 ]" + Fore.RESET + " for square area calculation >
+
     if Choice == 2:
         ThreeDimAreaCalc()
-        time.sleep(120) # This is a delay for countdown until the program exits (After the script has run. 2 min length)
+        input(Fore.RESET + 'press enter to exit: ')
+
     elif Choice == 1:
         TwoDimAreaCalc()
-        time.sleep(120) # This is a delay for countdown until the program exits (After the script has run. 2 min length)
+        input(Fore.RESET + 'press enter to exit: ')
+
     else:
         print("Choose a valid option (1 or 2)")
 
